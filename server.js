85623faee2651198598c12cff746675e82ddedb5
Glitch (fcc-apimicro-5) <none>
2020-06-23 10:23:57 +0000

    🛩😽 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+var multer = require("multer")
+var upload = multer({dest: "uploads/"});
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post('/api/fileanalyse',upload.single("upfile"), function(req, res){
+
+  const fileData = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+
+  res.json(fileData);
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
